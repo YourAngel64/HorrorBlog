e1c05cc64e766075ba326ff051dd7560214eb168
Enten <angelyadiel8776@gmail.com>
2024-04-16 22:47:22 +0000

    Blog 1 main finsihed

--- src/blog1.jsx
+++ src/blog1.jsx
@@ -1,5 +1,7 @@
 import "../public/CSS/Blog/1.css"
 
+
+//Try to make a static background where the backgrond covers the whole thing but whenever the scrollbar is used it doesnt move. it stays the same
 export function Blog_1(){
     return(
         <>
@@ -7,13 +9,27 @@ export function Blog_1(){
             {
             `
                 body{
-                    background-image: url('../public/Pictures/blog/ISTHISTHEPLACE.png');
-                    background-size: cover;
+                    background-color: #2ea4e8;
                 }
+
+                
             `
             }
             </style>
 
+            <div className="blog1_main">
+                
+                <div id="div_button">
+                    <a href="/posts" className="back_button"><p className="back_button"><u>Back</u></p></a>
+                </div>
+                
+                <br />
+                
+
+                <h1 id="title"><u>The Neighborhood</u></h1>
+
+                
+            </div>
             
         </>
     )
